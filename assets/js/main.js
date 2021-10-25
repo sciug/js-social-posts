d836043d604db2970a6e661fcc3c9dca134f3dda
sciug <gaetano.sciurti@libero.it>
2021-10-25 17:41:39 +0200

    add and remove like on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,8 @@ const posts = [
     {author: "Rose Mary",
     authorAvatar: "https://unsplash.it/300/300?image=",
     postImage: "https://unsplash.it/400/300?image=",
-    likes: 0,
-    postDate: "today",
+    likes: Math.floor(Math.random()*10),
+    postDate: new Date(),
     id: "firstPost",
     postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
     },
@@ -20,7 +20,7 @@ const posts = [
     {author: "John Doe",
     authorAvatar: "https://unsplash.it/700/700?image=",
     postImage: "https://unsplash.it/500/300?image=",
-    likes: 0,
+    likes: Math.floor(Math.random()*10),
     postDate: "today",
     id: "secondPost",
     postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
@@ -29,7 +29,7 @@ const posts = [
     {author: "Ruby Tuesday",
     authorAvatar: "https://unsplash.it/600/600?image=",
     postImage: "https://unsplash.it/600/300?image=",
-    likes: 0,
+    likes: Math.floor(Math.random()*10),
     postDate: "yesterday",
     id: "thirdPost",
     postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
@@ -37,7 +37,7 @@ const posts = [
 
 ]
 
-
+const likedPost =[]
 
 
 for(let i = 0; i <posts.length; i++){
@@ -45,7 +45,7 @@ for(let i = 0; i <posts.length; i++){
    
     const post = posts[i]
     const postElement = document.createElement("div")
-    postElement.classList.add("post", "mb-5", "p-3")
+    postElement.classList.add("post", "mb-5")
     postElement.setAttribute("id", `${post.id}`)
 
  
@@ -53,7 +53,7 @@ for(let i = 0; i <posts.length; i++){
 
     postElement.innerHTML = `<div class="info d-flex">
             <div class="author_img">
-            <img src="${post.authorAvatar}" class="img-fluid" alt="profile_pic">
+            <img src="${post.authorAvatar}" class="img-fluid" alt="profile_pic onerror="myFunction()">
         </div>
 
         <div class="author_date">
@@ -62,16 +62,23 @@ for(let i = 0; i <posts.length; i++){
         </div>
 
     </div>
+    <div class="post_text">
+    ${post.postContent}</div>
     <div class="post_image">
         <img src="${post.postImage}"  alt="">
     </div>
     <div class="likes d-flex align-items-center justify-content-between">
-        <button class="likeBtn">👍 mi piace</button>
+        <button class="likeBtn d-flex align-items-center"><span class="thumb">👍</span> <span>mi piace</span></button>
         <p class="m-0 likesNumber"></p>
     </div>
 `
 
+function myFunction(){
+   const profilePics= document.getElementsByClassName("author_img")
+   let profilePic = profilePics[i]
+   profilePic.innerHTML= post.author.charAt(0)
 
+}
 
 postContainer.append(postElement)
     
@@ -86,11 +93,34 @@ for(let i=0; i<likeBtns.length; i++){
     let likeBtn = likeBtns[i]
     let likeNumber = likeNumbers[i]
     likeNumber.innerHTML= "piace a " + posts[i].likes + " persone"
+
+
     likeBtn.addEventListener("click", function(){
+    
+        if(!likeBtn.classList.contains("green")){
+            
         posts[i].likes +=1
         likeNumber.innerHTML="piace a " + posts[i].likes + " persone"
-        console.log(posts[i].likes)
+        likeBtn.classList.add("green")
+        }else if(likeBtn.classList.contains("green")){
+            posts[i].likes -=1
+            likeNumber.innerHTML="piace a " + posts[i].likes + " persone"
+            likeBtn.classList.remove("green")
+
+        }
+
+
+
+        if(!likedPost.includes(posts[i].id)){
+        likedPost.push(posts[i].id)
+        console.log(likedPost)
+        }
 
 
     })
 }
+
+
+
+
+
