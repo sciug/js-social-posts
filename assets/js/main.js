b43d32b728285ae27f4ccd713698eac0542e022f
sciug <gaetano.sciurti@libero.it>
2021-10-25 14:13:36 +0200

    add html mock up

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const postContainer = document.getElementById("posts_container")
+
+
+
+
+
+const posts = [
+
+
+
+    {author: "Rose Mary",
+    authorAvatar: "https://unsplash.it/300/300?image=",
+    postImage: "https://unsplash.it/300/300?image=",
+    likes: 30,
+    postDate: "today",
+    id: "firstPost",
+    postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
+    },
+
+    {author: "John Doe",
+    authorAvatar: "https://unsplash.it/300/300?image=",
+    postImage: "https://unsplash.it/300/300?image=",
+    likes: 30,
+    postDate: "today",
+    id: "secondPost",
+    postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
+    },
+
+    {author: "Ruby ttsday",
+    authorAvatar: "https://unsplash.it/300/300?image=",
+    postImage: "https://unsplash.it/300/300?image=",
+    likes: 30,
+    postDate: "yesterday",
+    id: "thirdPost",
+    postContent: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque inventore modi illo eaque cum, nam rem? Dolorem, sunt optio? Magni consequatur provident ea asse velit!"
+    }
+
+]
+
+
+
+for(let i = 0; i <posts.length; i++){
+    const post = posts[i]
+
+    const postElement = document.createElement("div")
+  
+    
+
+    
+
+}
